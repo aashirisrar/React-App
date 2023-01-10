9501e189ad2c2bcaa8819a8480146e131191c0ed
Aashir Israr <aashirisrar968@gmail.com>
2023-01-10 22:09:17 +0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css'
-import Counter from './components/counter'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Counter />
+    <App />
   </React.StrictMode>
 );
 
