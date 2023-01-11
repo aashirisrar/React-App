b7e4d41b4fc5e731ee4defb32426cffcb311d390
Aashir Israr <aashirisrar968@gmail.com>
2023-01-11 22:44:05 +0500

    made a delete button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,10 +2,14 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value,
+    value: this.props.counter.value,
   };
 
   handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
+  };
+
+  handleDelete = () => {
     this.setState({ value: this.state.value + 1 });
   };
 
@@ -13,13 +17,20 @@ class Counter extends Component {
     console.log("props", this.props);
     return (
       <div>
-        <h4>{this.props.id}</h4>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={this.handleIncrement}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => {
+            this.props.onDelete(this.props.counter.id);
+          }}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
